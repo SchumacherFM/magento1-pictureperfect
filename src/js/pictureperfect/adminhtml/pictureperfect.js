6bddbd02c2baa3e87bcb50d34304a204dff32213
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-11-11 08:43:25 +1100

    [FEATURE] update get color method

--- src/js/pictureperfect/adminhtml/pictureperfect.js
+++ src/js/pictureperfect/adminhtml/pictureperfect.js
@@ -265,11 +265,12 @@
             // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
             // Adam Cole, 2011-Sept-14
             // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-            var r, g, b;
-            var h = step / numOfSteps;
-            var i = ~~(h * 6);
-            var f = h * 6 - i;
-            var q = 1 - f;
+            var
+                r, g, b,
+                h = step / numOfSteps,
+                i = ~~(h * 6),
+                f = h * 6 - i,
+                q = 1 - f;
             switch (i % 6) {
                 case 0:
                     r = 1, g = f, b = 0;
